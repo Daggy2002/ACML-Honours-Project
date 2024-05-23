5eedeb53cba5a6c1b700decdb1e4c4a49e38e86f
Sayfullah <sayfullah.jumoorty@live.com>
2024-05-23 21:27:42 +0200

    jobs

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 # This script is used to submit the batch files to the cluster
 
-sbatch job1.batch
+# run `sbatch job1.batch` 15 times, with a 1 second delay between each submission
+
+for i in {1..15}
+do
+    sbatch job.batch
+    sleep 1
+done
